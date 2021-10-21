b49f28faa046dd34f93c6e34138e012cf8bbdb06
Harsh Srivastava <harsh25097@gmail.com>
2021-10-21 12:46:27 +0530

    Fix Reuirment.txt

--- rain_prediction/settings.py
+++ rain_prediction/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
